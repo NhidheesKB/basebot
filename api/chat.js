55edb8995f54196eb5413e097e6a91c72b4606ed
NhidheesKB <nhidhees@gmail.com>
2025-09-09 14:03:18 +0530

    add error in chat

--- api/chat.js
+++ api/chat.js
@@ -33,5 +33,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ tabledData, message: tablemessage });
   } catch (error) {
     console.log("chaterror", error);
+    res.status(400).json({success:false})
   }
 }
